5b759e7439a20b6dd4ee649a19b496dd81dfd9a3
Olya <kandikowa.ol@yandex.ru>
2021-06-30 00:57:20 +0300

    blog-add

--- src/main/java/com/itprogect/blog/models/Post.java
+++ src/main/java/com/itprogect/blog/models/Post.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Post<text> {
+public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -53,4 +53,14 @@ public class Post<text> {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public Post(){}
+
+    public Post(String title, String anons, String full_text) {
+        this.title = title;
+        this.anons = anons;
+        this.full_text = full_text;
+
+    }
+
 }
